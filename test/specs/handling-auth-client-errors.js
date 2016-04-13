6d07056867b01ac221cd2b24faac8f2986f7d8c2
Alex Vernacchia <avernacchia@salesforce.com>
2016-04-13 10:48:43 +0100

    tests/handle auth client errors

---
+++ test/specs/handling-auth-client-errors.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016, Salesforce.com, Inc.
+ * All rights reserved.
+ *
+ * Legal Text is available at https://github.com/forcedotcom/Legal/blob/master/License.txt
+ */
+
+'use strict';
+var assert     = require('assert');
+var proxyquire = require('proxyquire');
+var sinon      = require('sinon');
+var FuelSoap;
+
+describe('handling errors from auth client in soapRequest', function() {
+	var deliverResponseSpy;
+	var initOptions;
+
+	beforeEach(function() {
+		deliverResponseSpy = sinon.spy();
+
+		initOptions = {
+			auth: {
+				clientId: 'testing'
+				, clientSecret: 'testing'
+			}
+		};
+
+		FuelSoap = proxyquire('../../lib/fuel-soap', {
+			'./helpers': {
+				deliverResponse: deliverResponseSpy
+			}
+		});
+	});
+
+	it('should tell helpers to deliver error response when auth client returns error on access token fetch', function() {
+		// Arrange
+		var soapClient = new FuelSoap(initOptions);
+		sinon.stub(soapClient.AuthClient, 'getAccessToken', function(options, cb) {
+			cb({ err: true }, null);
+		});
+
+		// Act
+		soapClient.soapRequest({}, function() {});
+
+		// Assert
+		assert.ok(deliverResponseSpy.calledOnce);
+	});
+
+	it('should tell helpers to deliver error response when auth client returns body without an access token', function() {
+		// Arrange
+		var soapClient = new FuelSoap(initOptions);
+		sinon.stub(soapClient.AuthClient, 'getAccessToken', function(options, cb) {
+			cb(null, {});
+		});
+
+		// Act
+		soapClient.soapRequest({}, function() {});
+
+		// Assert
+		assert.ok(deliverResponseSpy.calledOnce);
+	});
+});
